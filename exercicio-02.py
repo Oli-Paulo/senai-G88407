5d8c8ee61488b46e1202dfd4edbf3441d3b9deae
Oli-Paulo <pauloeduardo9941@gmail.com>
2023-11-22 21:23:19 -0300

    commit exercicio-02

---
+++ exercicio-02.py
@@ -0,0 +1,15 @@
+idade = int(input("Qual a sua idade? "))
+humano = input("Você é um humano? ").upper()
+
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Tu é menor.")
+
+if humano == "SIM":
+    humano == True
+    print("Tu é um humano")
+else:
+    humano == False
+    print("Tu não é um humano")
+    
