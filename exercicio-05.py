1913709c83366d92fa2223a5b972fbd56b6b0f48
Oli-Paulo <pauloeduardo9941@gmail.com>
2023-11-29 20:52:39 -0300

    exercicio-5.1

--- exercicio-05.py
+++ exercicio-05.py
@@ -1,13 +1,44 @@
+def login(dicionario):
+    nome = input("Nome do usuário: ")
+    senha = int(input("Senha do usuário: "))
+
+    if nome in Usuarios and Usuarios[nome] == senha:
+        print("Usuário e senha corretos!")
+        return True
+    else:
+        print("Usuário ou senha inválidos!")
+        return True
+
+def criarUsuario():
+        criarUsuario = input("Nome desejado: ")
+        criarSenha = int(input("Crie a senha apenas com números: "))
+        Usuarios[criarUsuario] = criarSenha
+        return True
+
+
 Usuarios = {
-    "PAULO": 12345,
-    "VICTOR": 54321,
-    "ALAN": 67890
+    "Paulo": 12345,
+    "Victor": 54321,
+    "Alan": 67890
 }
 
-nome = input("Nome do usuário: ").upper()
-senha = int(input("Senha do usuário: "))
+status = True
+while status == True:
+    opcoes = input("""
+    Escolha a opção desejada:\n
+    1 = Login  2 = Criar conta  3 = sair
+    """)
 
-if nome in Usuarios and Usuarios[nome] == senha:
-    print("Usuário e senha corretos!")
-else:
-    print("Usuário ou senha inválidos!")
+    if opcoes == "1":
+        status = login(Usuarios)
+
+    elif opcoes == "2":
+        status = criarUsuario()
+    
+    elif opcoes == "3":
+        print("Obrigado por acessar!")       
+        status = False
+
+    else:
+        print("Opção não encontrada.")
+        status = True
